6231bf0a3494166f8fe11295b376ed5d3243e904
RashmiChava <89612173+RashmiChava@users.noreply.github.com>
2022-04-07 14:57:46 -0500

    partial

---
+++ Chava_BookCheckoutApp/Chava_BookCheckoutApp/ViewController.swift
@@ -0,0 +1,61 @@
+//
+//  ViewController.swift
+//  Chava_BookCheckoutApp
+//
+//  Created by student on 4/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var usernameOutlet: UITextField!
+    
+    @IBOutlet weak var bookIdOutlet: UITextField!
+    
+    @IBOutlet weak var Imageoutlet: UIImageView!
+    @IBOutlet weak var BookFound: UILabel!
+    
+    var bookfound = Book()
+    
+    var isBook = false
+    
+    var BookArray = booksArray
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    
+    
+    @IBAction func BookAction(_ sender: UIButton) {
+        
+        let enteredId = bookIdOutlet.text!
+        
+        for book in BookArray {
+            if enteredId == book.bookId{
+                bookfound = book
+                isBook = true
+                
+                BookFound.text = "Book with ID" + enteredId + "found!"
+                //Imageoutlet.image = UIImage(book.bookImage)
+                
+                
+                
+            }
+            else{
+                BookFound.text = "Book not Found!"
+            }
+            
+            
+                
+                
+                
+        }
+    }
+    
+
+    
+}
+
---
+++ Chava_BookCheckoutApp/Chava_BookCheckoutAppUITests/Chava_BookCheckoutAppUITestsLaunchTests.swift
@@ -0,0 +1,32 @@
+//
+//  Chava_BookCheckoutAppUITestsLaunchTests.swift
+//  Chava_BookCheckoutAppUITests
+//
+//  Created by student on 4/7/22.
+//
+
+import XCTest
+
+class Chava_BookCheckoutAppUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
